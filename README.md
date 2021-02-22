d02bb6770a470aa738676b8ebfc8de1b81783320
Raj Lad <rajlad414@gmail.com>
2021-02-22 10:54:53 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# music_beats
+
+It's a music app
+
+Its a static music app with wonderful UI design in flutter.
+
+
+
---
+++ android/app/src/main/kotlin/example/com/music_beats/MainActivity.kt
@@ -0,0 +1,6 @@
+package example.com.music_beats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
